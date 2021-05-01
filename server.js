3218a911dd671433f1846369f271992ab3edbfda
aegli84 <egli.andreea@yahoo.com>
2021-05-01 22:07:19 +0200

    v1

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose= require('mongoose')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,  useUnifiedTopology: true 
+})
+
+
+//set view engine
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+//create route
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test description 2'
+    }]
+    res.render('articles/index', { articles: articles })
+})
+
+app.listen(5000)
