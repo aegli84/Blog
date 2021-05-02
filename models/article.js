67263bf24abcbb1ca8a55b9477bd14fde58c1158
aegli84 <egli.andreea@yahoo.com>
2021-05-02 16:34:02 +0200

    v1.1

--- models/article.js
+++ models/article.js
@@ -1,1 +1,46 @@
-// 25:38
+const mongoose = require('mongoose')
+const marked = require('marked') // library which allows to create markdown and turn it into html
+const slugify = require('slugify') // library which allows to turn smth like a title into a url friendly slug which can be used instead of a title in the url
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)// this allows to create html and purify it
+
+//this is a database
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
+    }
+})
+
+articleSchema.pre('validate', function(next) {
+    if (this.title) {
+        this.slug = slugify(this.title, { lower: true, strict: true})
+    }
+
+    if(this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    }
+    next()
+})
+module.exports = mongoose.model('Article', articleSchema)
